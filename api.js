487cce9c6aa5d2684db82d41bd667c7a722adef2
eduu25 <100290358+eduu25@users.noreply.github.com>
2022-04-11 20:24:25 -0300

    tratamento ajax api

--- api.js
+++ api.js
@@ -3,7 +3,17 @@ $("#mostrarVd").hide();
 
 $("#pesquisar").click(function (event) {
     event.preventDefault();
-    let pesData = $("#dtInput").val()
+    let pesquisaData = $("#dtInput").val()
+    apiNasa(pesquisaData)
 
 })
 
+
+function apiNasa(pesquisaData){
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod? api_key=oveUlZprz2uvkYMLThEDMESx06hEXWrRdvcxqA7c=${pesquisaData}`,
+        success: function(userResposta){
+
+        }
+    })
+}
