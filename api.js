e539cf63a08e0d38d305a55aa46e0cda65a74418
eduu25 <100290358+eduu25@users.noreply.github.com>
2022-04-13 22:29:30 -0300

    condicionais para img, e video

--- api.js
+++ api.js
@@ -11,8 +11,26 @@ $("#pesquisar").click(function (event) {
 
 function apiNasa(pesquisaData){
     $.ajax({
-        url: `https://api.nasa.gov/planetary/apod? api_key=oveUlZprz2uvkYMLThEDMESx06hEXWrRdvcxqA7c=${pesquisaData}`,
+        url: `https://api.nasa.gov/planetary/apod?api_key=oveUlZprz2uvkYMLThEDMESx06hEXWrRdvcxqA7c&date=${pesquisaData}`,
         success: function(userResposta){
+            if(userResposta.media_type == "image"){
+                $("#trocarNome").text("Foto:");
+                $("#mostrarVd").hide();
+                $("#mostrarImg").show();
+                $("#Nome").text(userResposta.title)
+                $("#mostrarImg").attr("src", userResposta.url);
+            }
+            else{
+                $("#trocarNome").text("Video:");
+                $("#mostrarImg").hide();
+                $("#mostrarVd").show();
+                $("#Nome").text(userResposta.title);
+                $("#mostrarVd").attr("src", userResposta.url);
+
+
+
+
+            }
 
         }
     })
