b7f9fa34572b3c383d54abd62554875a4a0dff29
eduu25 <100290358+eduu25@users.noreply.github.com>
2022-04-14 20:15:46 -0300

    js otimizado

--- api.js
+++ api.js
@@ -1,6 +1,3 @@
-$("#mostrarImg").hide();
-$("#mostrarVd").hide();
-
 $("#pesquisar").click(function (event) {
     event.preventDefault();
     let pesquisaData = $("#dtInput").val()
@@ -8,20 +5,19 @@ $("#pesquisar").click(function (event) {
 
 })
 
-
 function apiNasa(pesquisaData){
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=oveUlZprz2uvkYMLThEDMESx06hEXWrRdvcxqA7c&date=${pesquisaData}`,
         success: function(userResposta){
             if(userResposta.media_type == "image"){
-                $("#trocarNome").text("Foto:");
+                $("#trocarNome").text("Foto");
                 $("#mostrarVd").hide();
                 $("#mostrarImg").show();
                 $("#Nome").text(userResposta.title)
                 $("#mostrarImg").attr("src", userResposta.url);
             }
             else{
-                $("#trocarNome").text("Video:");
+                $("#trocarNome").text("Video");
                 $("#mostrarImg").hide();
                 $("#mostrarVd").show();
                 $("#Nome").text(userResposta.title);
